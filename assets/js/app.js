af8e9f42684dfd91011e3677f8ca9ce950e1a1ba
Bparthib <bparthib77@gmail.com>
2021-11-02 01:00:19 +0600

    third commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-$(function () {});
+$(function () {
+    // Cards Click JS Start
+    $('.viewcard').on('click', function(){
+        $('.views').addClass('block'),
+        $('.subscribers').removeClass('block'),
+        $('.comments').removeClass('block'),
+        $('.earnings').removeClass('block')
+    });
+
+    $('.subscribercard').on('click', function(){
+        $('.subscribers').addClass('block'),
+        $('.views').removeClass('block'),
+        $('.comments').removeClass('block'),
+        $('.earnings').removeClass('block')
+    });
+
+    $('.commentcard').on('click', function(){
+        $('.comments').addClass('block'),
+        $('.views').removeClass('block'),
+        $('.subscribers').removeClass('block'),
+        $('.earnings').removeClass('block')
+    });
+
+    $('.earningcard').on('click', function(){
+        $('.earnings').addClass('block'),
+        $('.views').removeClass('block'),
+        $('.comments').removeClass('block'),
+        $('.subscribers').removeClass('block')
+    });
+    // //Cards Click JS End
+});
